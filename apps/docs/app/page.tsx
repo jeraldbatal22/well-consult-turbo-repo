a8540d87a3a261a180efa7d7ee91c457284b78f9
Jerald Batal <jebatal@highlysucceed.com>
2025-11-13 17:10:40 +0800

    Update both docs and web

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/components/ui/button";
 export default function Home() {
   return (
     <div className=" h-screen flex justify-center items-center">
-      <Button className="bg-blue-500">HELLO FROM ADMIN SIDE</Button>
+      <Button className="bg-blue-500">HELLO FROM ADMIN SIDE UPDATE</Button>
     </div>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/components/ui/button";
 export default function Home() {
   return (
     <div className=" h-screen flex justify-center items-center">
-      <Button className="bg-green-500">HELLO FROM USER SIDE</Button>
+      <Button className="bg-green-500">HELLO FROM USER SIDE UPDATE</Button>
     </div>
   );
 }
